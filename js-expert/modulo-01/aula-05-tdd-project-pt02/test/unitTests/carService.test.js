92bcc3f0bfd3891304e08a5b2bb3facbde349afd
Mlodovico <murilo.lodovico@outlook.com>
2025-09-14 23:55:04 -0300

    feature: pt 2 mocks and data mock

--- js-expert/modulo-01/aula-05-tdd-project-pt02/test/unitTests/carService.test.js
+++ js-expert/modulo-01/aula-05-tdd-project-pt02/test/unitTests/carService.test.js
@@ -2,8 +2,15 @@ const { describe, it, before} = require('mocha');
 const { join } = require('path');
 
 const CarService = require('../../../src/entities/services/carServices');
+const { assert } = require('console');
 
 const carsDatabase = join(__dirname, '../../../database', 'cars.json');
+
+const mocks = {
+    validCarCategory: require('./../mocks/valid-carCategory.json'),
+    validCar: require('./../mocks/valid-car.json'),
+    validCustomer: require('./../mocks/valid-customer.json'),
+}
 
 describe('CarService', () => {
     let carService = {};
@@ -12,9 +19,9 @@ describe('CarService', () => {
             cars: carsDatabase
         });
     });
-    it('test', async () => {
-        const result = carService.test();
-        console.log('result', result);
-        // expect(result).to.be.deep.equal();
+    it('given a car category it should return an available car', async () => {
+        const car = mocks.validCar;
+        const carCategory = Object.create(mocks.validCarCategory);
+        
     });
 });
