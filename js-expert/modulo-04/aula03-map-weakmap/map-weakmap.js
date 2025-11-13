2aa9cf98c04e85467873c6f9c037c0cd3c7305cf
Mlodovico <murilo.lodovico@outlook.com>
2025-11-13 00:11:23 -0300

    feature: weakMap and Map

--- js-expert/modulo-04/aula03-map-weakmap/map-weakmap.js
+++ js-expert/modulo-04/aula03-map-weakmap/map-weakmap.js
@@ -31,3 +31,36 @@ assert.ok(myMap.has(onlyReferenceWorks));
 
 // para deletar o item do map
 assert.ok(myMap.delete(onlyReferenceWorks));
+
+// Nao da para iterar em Objects diretamente
+// tem que transformar com o Object.entries(item)
+assert.deepStrictEqual(JSON.stringify([...myMap]), JSON.stringify([[1, "one"], ["Erick", { "text": "two"}], [true, () => {}]]))
+
+const actor = {
+    name: 'Erick Wendel',
+    toString: "Queen's Gambit"
+}
+
+myMap.set(actor);
+
+assert.ok(myMap.has(actor));
+assert.throws(() => myMap.get({ name: 'Erick Wendel', toString: "Queen's Gambit" }));
+
+// limpar o map 
+myMap.clear();
+assert.deepStrictEqual(myMap.size, 0);
+
+// ---------------------------------------------------- 
+
+// WeakMap
+// Pode ser coletado após perder as referencias
+// usado em casos beeeeeem especificos
+// MAS: Ñ é iterável, não tem size, não tem clear
+// mais leve e preve o memorie leak, porque depois que as instancias saem da memroia, tudo é limpo
+
+const weakMap = new WeakMap();
+const hero = { name: 'Flash' };
+
+weakMap.set(hero);
+weakMap.get(hero);
+weakMap.delete(hero);
